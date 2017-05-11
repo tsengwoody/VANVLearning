90005288cd4f98df93eeccc60e8f4b28b44139b4
tsengwoody <tsengwoody.tw@gmail.com>
2017-05-11 11:53:25 +0800

    1. 加入以json取得表單資訊

--- MaterialADocument/urls.py
+++ MaterialADocument/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^material/create/$', views.material_create, name='material_create'),
 	url(r'^material/creategroup/$', views.material_creategroup, name='material_creategroup'),
+	url(r'^get_form_info/(?P<type>\w+)$', views.get_form_info, name='get_form_info'),
 ]
--- MaterialADocument/views.py
+++ MaterialADocument/views.py
@@ -17,3 +17,27 @@ def material_create(request, template_name='MaterialADocument/material_create.ht
 
 def material_creategroup(request, template_name='MaterialADocument/material_creategroup.html'):
 	return render(request, template_name, locals())
+
+def get_form_info(request, template_name='MaterialADocument/get_form_info.html', *args, **kwargs):
+	if kwargs['type'] == 'TrueFalseForm':
+		form = TrueFalseForm()
+	from collections import defaultdict
+	field_info = defaultdict(dict)
+	for id, field in enumerate(form):
+		field_info[id]['name'] = field.name
+		field_info[id]['value'] = field.value()
+		field_info[id]['type'] = field.field.widget.__class__.__name__
+		field_info[id]['attrs'] = field.field.widget.attrs
+		field_info[id]['label'] = field.field.label
+		field_info[id]['help_text'] = field.field.help_text
+		try:
+			field_info[id]['choices'] = field.field.widget.choices
+		except:
+			pass
+	response = field_info
+	import json
+	s = json.dumps(response)
+	if request.is_ajax():
+		return HttpResponse(json.dumps(response), content_type="application/json")
+	else:
+		return render(request, template_name, locals())
